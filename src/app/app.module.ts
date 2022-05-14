3d6e2396b92b5e05f929cb81ba1f6e5ce1b371a2
cristianjofre <megalocoman@gmail.com>
2022-05-14 15:33:32 -0400

    first commit basic proyect

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { RecipesComponent } from './Features/recipes/recipes.component';
+import { RecipelistComponent } from './Features/recipes/recipelist/recipelist.component';
+import { RecipeItemComponent } from './Features/recipes/recipelist/recipe-item/recipe-item.component';
+import { ShoppinglistComponent } from './Features/shoppinglist/shoppinglist.component';
+import { ShoppingeditComponent } from './Features/shoppinglist/shoppingedit/shoppingedit.component';
+import { RecipedetailComponent } from './Features/recipedetail/recipedetail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipesComponent,
+    RecipelistComponent,
+    RecipeItemComponent,
+    ShoppinglistComponent,
+    ShoppingeditComponent,
+    RecipedetailComponent
   ],
   imports: [
     BrowserModule
