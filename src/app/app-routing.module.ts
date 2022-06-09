985c7a7c630f451abd1fcb7e0cb57f78c9fdd7eb
cristianjofre <megalocoman@gmail.com>
2022-06-09 12:42:28 -0400

    component modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { AuthComponent } from './auth/auth.component';
 
 const appRoutes: Routes = [
-  { path: '', redirectTo: '/recipe', pathMatch: 'full' },
-  { path: 'auth', component: AuthComponent },
+  { path: '', redirectTo: '/recipe', pathMatch: 'full' }
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,37 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { ShoppingListServiceService } from './Service/shopping-list-service.service';
 import { AppRoutingModule } from './app-routing.module';
-import { RecipeService } from './Service/recipe.service';
 import { AuthComponent } from './auth/auth.component';
-import { AuthInterceptorService } from './auth/auth-interceptor.service';
 import { RecipesModule } from './Features/recipes/recipes.module';
 import { ShoppingListModules } from './Features/shoppinglist/shopping-edit.module';
 import { SharedModule } from './shared/shared.module';
+import { CoreModule } from './core.module';
+import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    AuthComponent
-
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
     AppRoutingModule,
-    ReactiveFormsModule,
     HttpClientModule,
     RecipesModule,
     ShoppingListModules,
-    SharedModule
+    SharedModule,
+    CoreModule,
+    AuthModule
   ],
-  providers: [ShoppingListServiceService, RecipeService, { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true }],
   bootstrap: [AppComponent]
   // only for version under 9 or older
   // ,
